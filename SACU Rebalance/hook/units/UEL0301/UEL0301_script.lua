ffdadad794e67626edeeb51ac8f2f34809195ac6
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2025-01-26 02:14:08 -0800

    Init UEF ShieldGeneratorField upgrade
    Update UEL0301_script.lua
    
    Update UEL0301_unit.bp

--- SACU Rebalance/hook/units/UEL0301/UEL0301_script.lua
+++ SACU Rebalance/hook/units/UEL0301/UEL0301_script.lua
@@ -2,6 +2,25 @@ local oldUEL0301 = UEL0301
 
 ---@class UEL0301_new : UEL0301
 UEL0301 = ClassUnit(oldUEL0301) {
+    ---@param self UEL0301_new
+    ---@param bp UnitBlueprintEnhancement
+    ProcessEnhancementShieldGeneratorField = function(self, bp)
+        self:DestroyShield()
+        self:ForkThread(function()
+            WaitTicks(1)
+            self:CreateShield(bp)
+            self:SetEnergyMaintenanceConsumptionOverride(bp.MaintenanceConsumptionPerSecondEnergy or 0)
+            self:SetMaintenanceConsumptionActive()
+        end)
+    end,
+
+    ---@param self UEL0301_new
+    ---@param bp UnitBlueprintEnhancement unused
+    ProcessEnhancementShieldGeneratorFieldRemove = function(self, bp)
+        self:DestroyShield()
+        self:SetMaintenanceConsumptionInactive()
+        self:RemoveToggleCap('RULEUTC_ShieldToggle')
+    end,
 }
 
 TypeClass = UEL0301
