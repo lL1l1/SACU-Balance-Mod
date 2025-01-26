e7bd750c708653ac06b7cfdbbf9aad00371d7e35
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2025-01-25 20:07:35 -0800

    Create mod_info.lua

---
+++ SACU Rebalance/mod_info.lua
@@ -0,0 +1,20 @@
+name = "SACU Rebalance"
+version = 1
+copyright = ""
+description = "Implements proposed changes for the SACU rebalance. SACUs are overall cheaper, some upgrades are revamped, and they have a new upgrade for a support role meant to enhance a larger army."
+author = "Nomander, FAF Balance Team"
+url = ""
+github = ""
+uid = "f59400cb-c315-44cb-8d6d-fcfe56ac3b91"
+
+selectable = true
+enabled = true
+exclusive = false
+ui_only = false
+
+icon = "mod_icon.dds"
+-- requires = {}
+-- requiresNames = {}
+-- conflicts = {}
+-- before = {}
+-- after = {}
