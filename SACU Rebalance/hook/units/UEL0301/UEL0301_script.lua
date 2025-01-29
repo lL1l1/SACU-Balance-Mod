4e786330b0687e5bd0f168330f39ebb5db88bc01
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2025-01-28 20:39:03 -0800

    Combine UEF Jammer into Sensor
    This allows jamming/sensor + shield. It also removes the jammer as a useless upgrade.

--- SACU Rebalance/hook/units/UEL0301/UEL0301_script.lua
+++ SACU Rebalance/hook/units/UEL0301/UEL0301_script.lua
@@ -1,22 +1,84 @@
 local oldUEL0301 = UEL0301
 
+---@param self Unit
+---@param amount number
+local function AddToEnergyMaintOverride(self, amount)
+    if not amount then return end
+    local newEnergy = (self.EnergyMaintenanceConsumptionOverride or 0) + amount
+    self:SetEnergyMaintenanceConsumptionOverride(newEnergy)
+    if newEnergy > 0 then
+        if self.MaintenanceConsumption then
+            self:UpdateConsumptionValues()
+        else
+            self:SetMaintenanceConsumptionActive()
+        end
+    else
+        self:SetMaintenanceConsumptionInactive()
+    end
+end
+
 ---@class UEL0301_new : UEL0301
 UEL0301 = ClassUnit(oldUEL0301) {
+    ---@param self UEL0301
+    ---@param bp UnitBlueprintEnhancement
+    ProcessEnhancementShield = function(self, bp)
+        self:AddToggleCap('RULEUTC_ShieldToggle')
+        AddToEnergyMaintOverride(self, bp.MaintenanceConsumptionPerSecondEnergy)
+        self:CreateShield(bp)
+    end,
+
+    ---@param self UEL0301
+    ---@param bp UnitBlueprintEnhancement unused
+    ProcessEnhancementShieldRemove = function(self, bp)
+        RemoveUnitEnhancement(self, 'Shield')
+        self:DestroyShield()
+        AddToEnergyMaintOverride(self, -self.Blueprint.Enhancements["Shield"].MaintenanceConsumptionPerSecondEnergy)
+        self:RemoveToggleCap('RULEUTC_ShieldToggle')
+    end,
+
     ---@param self UEL0301_new
     ---@param bp UnitBlueprintEnhancement
-    ProcessEnhancementShieldGeneratorField = function (self, bp)
+    ProcessEnhancementShieldGeneratorField = function(self, bp)
         self:AddToggleCap('RULEUTC_ShieldToggle')
-        self:SetEnergyMaintenanceConsumptionOverride(bp.MaintenanceConsumptionPerSecondEnergy or 0)
-        self:SetMaintenanceConsumptionActive()
+        AddToEnergyMaintOverride(self, bp.MaintenanceConsumptionPerSecondEnergy)
         self:CreateShield(bp)
     end,
 
     ---@param self UEL0301_new
     ---@param bp UnitBlueprintEnhancement unused
-    ProcessEnhancementShieldGeneratorFieldRemove = function (self, bp)
+    ProcessEnhancementShieldGeneratorFieldRemove = function(self, bp)
         self:DestroyShield()
-        self:SetMaintenanceConsumptionInactive()
+        AddToEnergyMaintOverride(self, -self.Blueprint.Enhancements["ShieldGeneratorField"].MaintenanceConsumptionPerSecondEnergy)
         self:RemoveToggleCap('RULEUTC_ShieldToggle')
+    end,
+
+    ---@param self UEL0301_new
+    ---@param bp UnitBlueprintEnhancement
+    ProcessEnhancementSensorRangeEnhancer = function(self, bp)
+        oldUEL0301.ProcessEnhancementSensorRangeEnhancer(self, bp)
+        self:SetIntelRadius('Jammer', bp.NewJammerRadius or 26)
+        self:EnableUnitIntel('Enhancement', 'Jammer')
+        self:AddToggleCap('RULEUTC_JammingToggle')
+        AddToEnergyMaintOverride(self, bp.MaintenanceConsumptionPerSecondEnergy)
+        if self.IntelEffects then
+            self.IntelEffectsBag = {}
+            self:CreateTerrainTypeEffects(self.IntelEffects, 'FXIdle', self.Layer, nil, self.IntelEffectsBag)
+        end
+    end,
+
+    ---@param self UEL0301
+    ---@param bp UnitBlueprintEnhancement
+    ProcessEnhancementSensorRangeEnhancerRemove = function(self, bp)
+        oldUEL0301.ProcessEnhancementSensorRangeEnhancerRemove(self, bp)
+
+        self.RadarJammerEnh = false
+        self:SetIntelRadius('Jammer', 0)
+        self:DisableUnitIntel('Enhancement', 'Jammer')
+        self:RemoveToggleCap('RULEUTC_JammingToggle')
+        AddToEnergyMaintOverride(self, -self.Blueprint.Enhancements["SensorRangeEnhancer"].MaintenanceConsumptionPerSecondEnergy)
+        if self.IntelEffectsBag then
+            EffectUtil.CleanupEffectBag(self, 'IntelEffectsBag')
+        end
     end,
 
     ---@param self UEL0301_new
