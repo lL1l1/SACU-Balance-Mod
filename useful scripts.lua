622d554db1694c3a0aeab35fa1e7b4fa0d8e09b3
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2025-01-28 17:06:41 -0800

    Create useful scripts.lua

---
+++ useful scripts.lua
@@ -0,0 +1,29 @@
+-- Spawn all base SACU, gateways, T3 land HQs, ACUs, Paragon
+CreateUnitAtMouse('uab0304', 0,   14.98,  -14.97,  0.00000)
+CreateUnitAtMouse('ual0301', 0,   15.22,  -10.50,  0.00000)
+CreateUnitAtMouse('uab0301', 0,   14.98,    2.03,  0.00000)
+CreateUnitAtMouse('ual0001', 0,   15.22,   -5.43,  0.00000)
+CreateUnitAtMouse('uab0302', 0,   14.98,   10.03,  0.00000)
+--
+CreateUnitAtMouse('urb0301', 0,  -15.02,    2.03,  0.00000)
+CreateUnitAtMouse('urb0304', 0,  -15.02,  -14.97,  0.00000)
+CreateUnitAtMouse('url0301', 0,  -14.78,  -10.50,  0.00000)
+CreateUnitAtMouse('url0001', 0,  -14.78,   -6.43,  0.00000)
+CreateUnitAtMouse('urb0302', 0,  -15.02,   10.03,  0.00000)
+--
+CreateUnitAtMouse('uel0001', 0,   -4.78,   -6.43,  0.00000)
+CreateUnitAtMouse('ueb0301', 0,   -5.02,    2.03,  0.00000)
+CreateUnitAtMouse('ueb0304', 0,   -5.02,  -14.97,  0.00000)
+CreateUnitAtMouse('uel0301', 0,   -4.78,  -10.27,  0.00000)
+CreateUnitAtMouse('ueb0302', 0,   -5.02,   10.03,  0.00000)
+--
+CreateUnitAtMouse('xsb0301', 0,    4.98,    2.03,  0.00000)
+CreateUnitAtMouse('xsb0302', 0,    4.98,   10.03,  0.00000)
+CreateUnitAtMouse('xsb0304', 0,    4.98,  -14.97,  0.00000)
+CreateUnitAtMouse('xsl0301', 0,    5.22,  -10.50,  0.00000)
+CreateUnitAtMouse('xsl0001', 0,    5.22,   -6.43,  0.00000)
+--
+CreateUnitAtMouse('xab1401', 0,   -0.02,  -24.97,  0.00000)
+for i = 1, 100 do
+    CreateUnitAtMouse('xrb0304', 0,    0.00,    1.34, -0.00000)
+end
