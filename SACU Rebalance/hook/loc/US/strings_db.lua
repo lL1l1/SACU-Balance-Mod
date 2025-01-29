b6234b10a0f2d2b864f6b82b261ce88586ed9abd
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2025-01-28 20:42:10 -0800

    Rebalance sensor/jammer cost/range
    Significantly more expensive because it is a hard counter and is mobile to cloak/stealth. The cost is based off omni + t3 pgen costs.
    In return, the intel range is increased to better support the army: Omni 80 -> 100 (stand in front of fatboy to give max range), vision 45 -> 50 (slightly more vision to help Percivals from further away)

--- SACU Rebalance/hook/loc/US/strings_db.lua
+++ SACU Rebalance/hook/loc/US/strings_db.lua
@@ -1,6 +1,6 @@
 xsl0301_RAS_name = ""
 
-Unit_Description_0020="Equips the SACU with a radar jammer that creates false radar signatures (countered by omni sensors) and greatly expands the range of its standard sensor systems.\n\n+64 Omni Radius\n+15 Vision Radius\nJamming Blips = 10\nJamming Radius = 16"
+Unit_Description_0020="Equips the SACU with a radar jammer that creates false radar signatures (countered by omni sensors) and greatly expands the range of its standard sensor systems.\n\n+84 Omni Radius\n+24 Vision Radius\nJamming Blips = 10\nJamming Radius = 16"
 uel0301_IntelJammer_desc="SACU (Sensor/Jamming Preset)"
 uel0301_IntelJammer_help="Enhanced during construction with the Intel Support System"
 uel0301_IntelJammer_name=""
