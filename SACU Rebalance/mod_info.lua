87c72ddefe9e0912cf6a90a8fed996427542b85e
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2025-07-22 04:30:13 -0700

    mod_info GH link

--- SACU Rebalance/mod_info.lua
+++ SACU Rebalance/mod_info.lua
@@ -4,7 +4,7 @@ copyright = ""
 description = "Implements changes for the SACU rebalance proposed by balance team: cheaper SACUs, stronger adjacency/gateways, new army support enhancements.\nRambo SACUs are indepedently reworked with new weapons and upgrades to fill gaps in faction rosters."
 author = "Nomander, FAF Balance Team"
 url = ""
-github = ""
+github = "https://github.com/lL1l1/SACU-Balance-Mod"
 uid = "f59400cb-c315-44cb-8d6d-fcfe56ac3b91"
 
 selectable = true
